ac1f7a1e7aafa5b2a42ad8d1823ae74384675ad4
Ádám Liszkai <adam@liszkai.dev>
2026-01-19 04:07:56 +0100

    feat: add kotlin JVM to the plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "dev.buildit"
 pluginManagement {
     plugins {
         id("com.gradleup.shadow") version "9.3.1"
+        kotlin("jvm") version "2.3.0"
     }
     repositories {
         gradlePluginPortal()
