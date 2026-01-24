d5d4f70a267ccb9e6800d84dd3f064df7725379b
Ádám Liszkai <adam@liszkai.dev>
2026-01-24 20:11:22 +0100

    docs: update java basic example for 0.1.4-dev

---
+++ examples/hytale-java/settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "example.java"
+
+pluginManagement {
+    // includeBuild("../..") // <- Simulating JetPack
+    plugins {
+        id("dev.scaffoldit") version "0.1.4-dev"
+        id("com.gradleup.shadow") version "9.3.1"
+    }
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("dev.scaffoldit")
+}
+
+hytale {
+    manifest {
+        Group = "ScaffoldIt"
+        Name = "Example"
+        Main = "dev.scaffoldit.example.JavaExample"
+    }
+}
--- examples/hytale-java/src/main/java/dev/scaffoldit/example/JavaExample.java
+++ examples/hytale-java/src/main/java/dev/scaffoldit/example/JavaExample.java
@@ -1,4 +1,4 @@
-package dev.buildit.example;
+package dev.scaffoldit.example;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
@@ -10,11 +10,11 @@ import javax.annotation.Nonnull;
  * This class serves as the entrypoint for your plugin.
  * Use the setup method to register into game registries or add event listeners!
  */
-public class BuildItExample extends JavaPlugin {
+public class JavaExample extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public BuildItExample(@Nonnull JavaPluginInit init) {
+    public JavaExample(@Nonnull JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Initialized " + this.getName());
     }
